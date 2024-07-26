2e91d4a80dc2cca8dd85c39c8e52a07782eeab84
r-southworth <rachael.southworth@datagrove.com>
2024-07-26 11:30:33 -0400

    configure multiple sidebars

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,34 +1,68 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import starlightUtils from "@lorenzo_lewis/starlight-utils";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [starlight({
-    title: 'LearnGrove Support',
-    logo: {
-      src: './src/assets/LearnGroveLogo.svg'
-    },
-    social: {
-      github: 'https://github.com/datagrove/learngrove-support'
-    },
-	customCss: ['./src/tailwind.css'],
-    sidebar: [{
-      label: 'Guides',
-      items: [
-      // Each item here is one entry in the navigation menu.
-      {
-        label: 'Example Guide',
-        slug: 'guides/example'
-      }]
-    }, {
-      label: 'Reference',
-      autogenerate: {
-        directory: 'reference'
-      }
-    }]
-  }), tailwind({
-	applyBaseStyles: false,
-  })]
+	integrations: [
+		starlight({
+			title: 'LearnGrove Support',
+			logo: {
+				src: './src/assets/LearnGroveLogo.svg'
+			},
+			social: {
+				github: 'https://github.com/datagrove/learngrove-support'
+			},
+			customCss: ['./src/tailwind.css'],
+			sidebar: [
+				{
+					label: 'Creators',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{
+							label: 'Creators',
+							autogenerate: {
+								directory: 'creators'
+							}
+						},
+					]
+				}, {
+					label: 'Learners & Learning Facilitators',
+					items: [
+						{
+							label: 'Learners & Learning Facilitators',
+							autogenerate: {
+								directory: 'learners'
+							}
+						}
+					]
+				},
+				{
+					label: 'leadingNavLinks',
+					items: [
+						{
+							label: 'Creators',
+							link: '/creators'
+						},
+						{
+							label: 'Learning Facilitators',
+							link: '/learners'
+						}
+					]
+				},],
+			plugins: [
+				starlightUtils({
+					navLinks: {
+						leading: { useSidebarLabelled: "leadingNavLinks" }
+					},
+					multiSidebar: {
+						switcherStyle: "hidden",
+					},
+				}),
+			],
+		}), tailwind({
+			applyBaseStyles: false,
+		})]
 });
